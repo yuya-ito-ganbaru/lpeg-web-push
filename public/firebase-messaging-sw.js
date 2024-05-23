c46742528cda8304c95e58583dbbe1eae63692a9
yuya-ito-ganbaru <yuya.ito1990@gmail.com>
2024-05-23 23:30:48 +0900

    first commit

---
+++ public/firebase-messaging-sw.js
@@ -0,0 +1,19 @@
+importScripts('/__/firebase/9.2.0/firebase-app-compat.js');
+importScripts('/__/firebase/9.2.0/firebase-messaging-compat.js');
+importScripts('/__/firebase/init.js');
+
+const messaging = firebase.messaging();
+
+importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.2.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+	apiKey: "AIzaSyBHHlcMYnmSkMDq3E9zMjl4Z1Fdogc3E3U",
+	authDomain: "lpeg-web-push-5f284.firebaseapp.com",
+	projectId: "lpeg-web-push-5f284",
+	storageBucket: "lpeg-web-push-5f284.appspot.com",
+	messagingSenderId: "1085820492003",
+	appId: "1:1085820492003:web:b722de28f6ebdb22b54dd6",
+});
+
+
